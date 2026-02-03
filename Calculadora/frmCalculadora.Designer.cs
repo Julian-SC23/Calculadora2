ad443c648c9fc2a49fb58f5af4160b6c5f4f5ac1
fcoan <gonzalezbarbosasebastian@gmail.com>
2026-02-03 15:12:15 -0600

    Se agregan botones de calcular y reset al formulario de Calculadora

---
+++ Calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtVariableA = new TextBox();
+            txtVariableB = new TextBox();
+            btnCalcular = new Button();
+            btnReset = new Button();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(59, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Variable A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(59, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Variable B";
+            // 
+            // txtVariableA
+            // 
+            txtVariableA.Location = new Point(60, 45);
+            txtVariableA.Name = "txtVariableA";
+            txtVariableA.Size = new Size(196, 23);
+            txtVariableA.TabIndex = 2;
+            txtVariableA.TextChanged += txtVariableA_TextChanged;
+            // 
+            // txtVariableB
+            // 
+            txtVariableB.Location = new Point(56, 127);
+            txtVariableB.Name = "txtVariableB";
+            txtVariableB.Size = new Size(200, 23);
+            txtVariableB.TabIndex = 3;
+            txtVariableB.TextChanged += txtVariableB_TextChanged;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(490, 38);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(98, 30);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(492, 95);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(96, 30);
+            btnReset.TabIndex = 5;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(273, 23);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 127);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Operaciones";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(20, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(55, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Suma";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(20, 47);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(20, 70);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(94, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(20, 95);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(94, 19);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // frmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(635, 216);
+            Controls.Add(groupBox1);
+            Controls.Add(btnReset);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtVariableB);
+            Controls.Add(txtVariableA);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmCalculadora";
+            Text = "Calculadora";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtVariableA;
+        private TextBox txtVariableB;
+        private Button btnCalcular;
+        private Button btnReset;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+    }
+}
---
+++ Calculadora/frmCalculadora.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void txtVariableA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtVariableB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = 0, b = 0, resultado = 0;
+                a = Convert.ToDouble(this.txtVariableA.Text);
+                b = Convert.ToDouble(this.txtVariableB.Text);
+                resultado = a + b;
+                MessageBox.Show("El resultado es: " + resultado.ToString(), "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                reset();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error en la conversion de datos", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void reset()
+        {
+            txtVariableA.Text = "";
+            txtVariableB.Clear();
+            txtVariableA.Focus();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
